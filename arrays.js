be062b3ae95ea1d95c6d6497c84a8e21e6ab878e
Octowl <alsaff1987@gmail.com>
2024-11-04 08:13:46 +0300

    clean up instructions

--- arrays.js
+++ arrays.js
@@ -1,51 +1,65 @@
-// Step 1: Using createGroceries function return an array with 6 groceries items
+// Step 1:
+// Write a createGroceries function that returns an array with 6 groceries items
 function createGroceries() {
   // write your code here...
 }
 
-// Step 2: Using getSecondGroceryItem function return the second grocery item from `groceries` argument
+// Step 2:
+// Write a getSecondGroceryItem function that returns the second grocery item from `groceries` argument
 function getSecondGroceryItem(groceries) {
   // write your code here...
 }
 
-// Step 3: Using getGroceriesCount return the length of the `groceries` argument
+// Step 3:
+// Write a getGroceriesCount that returns the length of the `groceries` argument
 function getGroceriesCount(groceries) {
   // write your code here...
 }
 
-// Step 4: Using getLastGroceryItem function return the last grocery item from `groceries` argument
+// Step 4:
+// Write a getLastGroceryItem function that returns the last grocery item from `groceries` argument
 function getLastGroceryItem(groceries) {
   // write your code here...
 }
 
-// Step 5: Using removeLastGroceryItem remove the last grocery item and return it
+// Step 5:
+// Write a removeLastGroceryItem function that removes the last grocery item and return it
 function removeLastGroceryItem(groceries) {
   // write your code here...
 }
 
-// Step 6: Using addNewGroceries function Add two new and different grocery items to the end of the groceries argument, and return the new array
+// Step 6:
+// Write a addNewGroceries function that adds two new and different grocery items to the end of the groceries argument
+// and returns the modified array
 function addNewGroceries(groceries, itemOne, itemTwo) {
   // write your code here...
 }
 
-// Step 7: Using getFirstThreeGroceryItems function return a new array named that include the first three grocery items
+// Step 7:
+// Write a getFirstThreeGroceryItems function that returns a new array that contains the first three grocery items
 function getFirstThreeGroceryItems(groceries) {
   // write your code here...
 }
 
 // 🌶️🌶️🌶️ **Challenge**
 
-// Step 1: Using deleteThirdItem function delete the third item from the groceries list, and return the groceries list
+// Step 1:
+// Write a deleteThirdItem function that deletes the third item from the groceries argument
+// and returns the modified groceries list
 function deleteThirdItem(groceries) {
   // write your code here...
 }
 
-// Step 2: Using insertItemAtBeginning function inserts a new grocery item at the beginning of the array, and return the new groceries list
+// Step 2:
+// Write a insertItemAtBeginning function that inserts a new grocery item at the beginning of the groceries argument
+// and returns the modified groceries array
 function insertItemAtBeginning(groceries, item) {
   // write your code here...
 }
 
-// Step 3: Using replaceFirstTwoItems function replace the first two elements with 'ketchup' and 'chili', and return the new groceries list
+// Step 3:
+// Write a replaceFirstTwoItems function that replace the first two elements with 'ketchup' and 'chili'
+// and return the modified groceries array
 function replaceFirstTwoItems(groceries) {
   // write your code here...
 }
